e435854292fd36b9907676ed8a446392d05437ff
Angélica Tartajo Vázquez <angtarvaz@alu.edu.gva.es>
2024-12-06 20:23:28 +0100

    Subir Ejercicio2 a gitHub

--- src/FranciscoMáñezPiqueres.java
+++
@@ -1,1 +0,0 @@
-No se nada de JS
--- src/HalseyPayaManyez.java
+++
@@ -1,7 +0,0 @@
-public class HalseyPayaManyez {
-    public static void main(String[] args) {
-        System.out.print("Haha hola :)");
-        System.out.print("Adios :P");
-    }
-
-}
