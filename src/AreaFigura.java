7a1b216d5a6f5563065f44936c5ad48dd7ffd2d8
Palackk <framanpiq@alu.edu.gva.es>
2024-12-10 18:37:35 +0100

    Error_Solucionado_Paco

--- src/AreaFigura.java
+++ src/AreaFigura.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class AreaFigura {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenido al calculador de áreas");
 
